3f32877d17bde7b08b33b18e10b0214330b3e0f2
Kerubel Solomon <kibi729@gmail.com>
2023-02-16 11:11:05 -0800

    op

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ int main(void)
 	printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 	printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
-	printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
+		printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
