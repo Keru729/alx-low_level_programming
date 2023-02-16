0e06db4b3ee588588a7a3a3d73f3ec3877ee5b98
Kerubel Solomon <kibi729@gmail.com>
2023-02-16 11:00:07 -0800

    op

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,8 +11,8 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("size of a char: %li  byte(s)\n", (unsigned long)sizeof(a));
-	printf("size of an int: %li  byte(s)\n", (unsigned long)sizeof(b));
+	printf("size of a char: %li byte(s)\n", (unsigned long)sizeof(a));
+	printf("size of an int: %li byte(s)\n", (unsigned long)sizeof(b));
 	printf("size of a long int: %li byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of a long long int: %li byte(s)\n", (unsigned long)sizeof(d));
 	printf("size of a float: %li byte(s)\n", (unsigned long)sizeof(e));
