bafa822f6f5ea1463884ca8049b6db21cc01fd8c
Kerubel Solomon <kibi729@gmail.com>
2023-02-16 08:49:02 -0800

    po

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
-#include <studio.h>
+#include <stdio.h>
 int main (void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 		return (0);
 }
