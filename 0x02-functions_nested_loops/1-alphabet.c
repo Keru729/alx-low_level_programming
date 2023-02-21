e37c2dd049e4284bb61ec606d2b97273f6030867
Kerubel Solomon <kibi729@gmail.com>
2023-02-21 08:12:17 -0800

    lo

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabet in lowercase and a new line
+*/
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+
+	_putchar('\n');
+}
