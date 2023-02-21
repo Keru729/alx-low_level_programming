dfe6b992c3d8dd30a8ccc75d10ed36bb2c2cca11
Kerubel Solomon <kibi729@gmail.com>
2023-02-21 10:15:24 -0800

    ko

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -26,18 +26,18 @@ void jack_bauer(void)
 			{
 				m1 = 0;
 				while (m1 <= 9)
-			
-			_putchar('0' + h2);
-			_putchar('0' + h1);
-			_putchar(':');
-			_putchar('0' + m1);
-			_putchar('0' + m2);
-			_putchar('\n');
-			m1++;
+				{
+					_putchar('0' + h2);
+					_putchar('0' + h1);
+					_putchar(':');
+					_putchar('0' + m1);
+					_putchar('0' + m2);
+					_putchar('\n');
+					m1++;
+				}
+				m2++;
 			}
-			m2++;
+			h1++;
 		}
-		h1++;
+		h2++;
 	}
-	h2++;
-}
