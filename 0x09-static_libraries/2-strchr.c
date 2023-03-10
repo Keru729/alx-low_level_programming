a1aaf0a64481e8d4441533af93c3766536ffb6a7
Kerubel Solomon <kibi729@gmail.com>
2023-03-10 08:37:16 -0800

    static library

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ * Desc: Entry
+ *@s: pointer
+ *@c: string
+ * Return: Always 0 (Success)
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0' ; s++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+	}
+	if (s[i] == c)
+	{
+		return (s + i);
+	}
+	return (0);
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _islower - checkees lower case  character
+ * @c : Charcter to check the case
+ * Return: o or 1
+ */
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}
