ad2ad037fc62555644367d6f81cb5d3c83be60cf
Kerubel Solomon <kibi729@gmail.com>
2023-03-16 06:47:46 -0700

    p

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,17 @@
-0x0c malloca README.md
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - Main Entry
+ * @b: input
+ * Return: 0
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
