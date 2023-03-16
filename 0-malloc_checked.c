f2b84a567e1d9e9a2e7d9dce05ebae5d20029160
Kerubel Solomon <kibi729@gmail.com>
2023-03-16 06:29:59 -0700

    plo

--- 0-malloc_checked.c
+++ 0-malloc_checked.c
@@ -1,22 +1,18 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
-  * malloc_checked - Allocates memory
-  * @b: the size to allocate
-  *
-  * Return: Nothing.
-  */
+ * malloc_checked - Main Entry
+ * @b: input
+ * Return: 0
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *p;
 
 	p = malloc(b);
-
 	if (p == NULL)
 		exit(98);
-
 	return (p);
 }
 
