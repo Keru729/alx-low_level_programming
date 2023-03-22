3aa5f86cce3db91cb4d072341baf61f1345ece24
Kerubel Solomon <kibi729@gmail.com>
2023-03-22 08:26:22 -0700

    koil

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,48 +1,51 @@
-#include "3-calc.h"
 #include <stdio.h>
+#include "function_pointers.h"
 #include <stdlib.h>
+#include <string.h>
+
 /**
- * main - get_op_func has operators correlated with
- * func signs and funcs from op_func
- * if not 4 arguments, return Error & exit 98
- * if op is null, return Error & exit 99
- * if div or mod 0, return Error & exit 100
- * run calc, input one, operator, input two = pointer res to get_op
- * @argc: arguments
- * @argv: double pointer to arguments
- * Return: 0
+ * main - check the program
+ * @argc: arguments cant
+ * @argv: vector arguments
+ * Return: 0 if false, something else otherwise.
  */
 int main(int argc, char *argv[])
 {
-	int one, two, ans;
-	int (*res)(int, int);
-	char *get_op;
+	int (*get)(int, int);
+	int a, b, operator;
+
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	one = atoi(argv[1]);
-	two = atoi(argv[3]);
-	get_op = argv[2];
-
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
+	if (strlen(argv[2]) != 1)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
+	operator = argv[2][0];
+	switch (operator)
 	{
+	case '+':
+		break;
+	case '-':
+		break;
+	case '*':
+		break;
+	case '/':
+		break;
+	case '%':
+		break;
+	default:
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
 
-	res = get_op_func(get_op);
-	ans = res(one, two);
-
-	printf("%d\n", ans);
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	get = get_op_func(argv[2]);
+	printf("%d\n", get(a, b));
 	return (0);
 }
