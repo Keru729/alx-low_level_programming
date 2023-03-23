361a07a0a3cc5789219daf7eb06fb6bab3ffe261
Kerubel Solomon <kibi729@gmail.com>
2023-03-23 07:36:40 -0700

    ghi

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,29 +1,30 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
+
 /**
- * print_numbers - Entry Point
- * @separator: comma space
- * @n: elements to be printed
- * Return: void
- */
-void print_numbers(const char *separator, const unsigned int n, ...)
+  * sum_them_all - ...
+  * @n: ...
+  *
+  * Return: ...
+  */
+int sum_them_all(const unsigned int n, ...)
 {
-	char *sep;
-	unsigned int i;
-	va_list list;
+	va_list args;
+	unsigned int i = 0;
+	int sum = 0;
 
-	if (separator == NULL || *separator == 0)
-		sep = "";
-	else
-		sep = (char *) separator;
-	va_start(list, n);
+	if (n != 0)
+	{
+		va_start(args, n);
 
-	if (n > 0)
-		printf("%d", va_arg(list, int));
-	for (i = 1; i < n; i++)
-		printf("%s%d", sep, va_arg(list, int));
-	printf("\n");
-	va_end(list);
+		while (i < n)
+		{
+			sum += va_arg(args, int);
+			i++;
+		}
+
+		va_end(args);
+		return (sum);
+	}
+
+	return (0);
 }
-
